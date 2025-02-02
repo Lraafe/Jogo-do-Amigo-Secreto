b2e6be2ccef4a05309b6782d0420675bb6369e1f
Lraafe <Rafael31Alexandre@gmail.com>
2025-02-02 16:55:30 -0300

    Projeto Base Finalizado

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 var nomesSorteados = [];
+var nomesSorteadosM = [];
 
 function coletarNomes(){
     var nomes = document.querySelector("input").value;
     if(nomes == ""){
         alert("Por favor, insira um nome.");
     }else{  
-        console.log(nomes);
         nomesSorteados.push(nomes);
+        nomesSorteadosM.push(nomes);
         document.querySelector("input").value = "";
     }
-    console.log(nomesSorteados.length);
+}
+
+function atualizarLista(){
+    var lista = document.getElementById("listaAmigos");
+    var listaHtml = document.createElement("li");
+
+    listaHtml.textContent = nomesSorteadosM;
+    lista.appendChild(listaHtml);
+    nomesSorteadosM.pop();
+}
+
+function sortear(){
+    var numeroAleatorio;
+
+    if(nomesSorteados.length > 1){
+        numeroAleatorio = Math.floor(Math.random() * nomesSorteados.length);
+        var resultadoDoJogo = document.querySelector("h2");
+        resultadoDoJogo.innerHTML = `O seu amigo secreto -> ${nomesSorteados[numeroAleatorio]}`;
+    }else{
+        alert("Você não tem amigos o suficiente");
+    }
 }
